a51514ca54a9626f929fdf4d733f629564512760
TrudesEmerald <getrudekyeikbh@gmail.com>
2022-06-24 16:15:40 +0000

    1st commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+let name ="Getrude";
+let height = 5.5;
+let country = "Ghana";
+
+alert('I am ' + name + ' of ' + height +' feet ' +' '+  'living in ' + country);
+
+console.log('I am ' + name + ' of ' + height +' feet ' +' '+  'living in ' + country);
